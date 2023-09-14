9a3d25a944d435411cad33b66a5aa05f9a2ad15e
blockchainbrighton <jim@blockchainbrighton.com>
2023-09-14 12:05:16 +0100

    Tests

--- Audionals/tests/my-nft_test.ts
+++ Audionals/tests/my-nft_test.ts
@@ -11,21 +11,40 @@ import {
     name: "Ensure that NFT token URL and ID is as expected",
     async fn(chain: Chain, accounts: Map<string, Account>) {
       let wallet_1 = accounts.get("wallet_1")!;
+      
+      // Mint NFT with metadata
       let block = chain.mineBlock([
-        Tx.contractCall("my-nft", "get-last-token-id", [], wallet_1.address),
-        Tx.contractCall(
-          "my-nft",
-          "get-token-uri",
-          [types.uint(1)],
-          wallet_1.address
-        ),
+        Tx.contractCall("my-nft", "mint-with-metadata", [
+          types.ascii("https://token.uri"),
+          types.ascii("Qm123"), 
+          types.ascii("Strings"),
+          types.ascii("Acoustic Guitar"),
+          types.ascii("John Doe")
+        ], wallet_1.address),
       ]);
-      assertEquals(block.receipts.length, 2);
-      assertEquals(block.height, 2);
-      block.receipts[0].result.expectOk().expectUint(0);
-      block.receipts[1].result
+
+      // Get token data
+      block = chain.mineBlock([
+        Tx.contractCall("my-nft", "get-token-data", [types.uint(1)], wallet_1.address),
+      ]);
+
+      // Assert token data
+      block.receipts[0].result
         .expectOk()
         .expectSome()
-        .expectAscii("https://token.stacks.co/{id}.json");
+        .expectAscii("https://token.uri")
+        .expectAscii("Qm123")
+        .expectAscii("Strings")
+        .expectAscii("Acoustic Guitar")
+        .expectAscii("John Doe");
+
+      // Get token ID
+      block = chain.mineBlock([
+        Tx.contractCall("my-nft", "get-last-token-id", [], wallet_1.address),
+      ]);
+
+      // Assert token ID
+      block.receipts[0].result.expectOk().expectUint(1);
+      
     },
   });
